7f9de83437e20b7f2acf0ba6348bf63560671c90
ReporDiscord <tiendaonlinerh2@gmail.com>
2026-04-15 16:09:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
+// 🌐 Servidor Express (para Render)
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🤖 Bot Discord activo");
+});
+
+app.listen(3000, () => {
+  console.log("🌐 Web activa en puerto 3000");
+});
+
+// 🤖 Cliente Discord
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
